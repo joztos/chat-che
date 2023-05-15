d0822e50bbed501fc0841ce1d16abc07713b844b
Your Name <you@example.com>
2023-05-15 13:11:43 -0600

    Revert "trygpt4"
    This reverts commit 5e30c0bc1b3890d8c6f20e48b5494165ccaa3623.

--- ask-my-book-chatbot/components/Layout.tsx
+++ ask-my-book-chatbot/components/Layout.tsx
@@ -1,6 +1,7 @@
 import type { FC, ReactNode } from 'react'
 import Head from 'next/head.js'
 import Nav, { NavProps } from './Nav'
+import Logo from './Steamship'
 import { ComponentType } from 'react'
 
 export interface LayoutProps extends NavProps {
@@ -8,6 +9,8 @@ export interface LayoutProps extends NavProps {
   title?: string
   description?: string
 }
+
+
 
 const Layout: FC<LayoutProps> = ({
   title,
@@ -24,7 +27,31 @@ const Layout: FC<LayoutProps> = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Nav path={path} deployButton={deployButton} />
+
       <div className="px-8 bg-accents-0">{children}</div>
+
+      <footer className="py-10 w-full mt-auto border-t flex items-center justify-center bg-accents-1 z-20">
+        <span className="text-primary">Created with</span>
+        <a
+          href="https://steamship.com"
+          aria-label="Steamship.com Link"
+          target="_blank"
+          rel="noreferrer"
+          className="text-black "
+        >
+          <Logo
+            className="inline-block h-6 ml-2 text-primary"
+          /> <span className='text-blue-800'>Steamship</span>
+        </a>. Based on the
+        <a
+          href="https://github.com/steamship-core/vercel-examples/tree/main/ask-my-book-chatbot"
+          target="_blank"
+          rel="noreferrer"
+          className="text-black text-blue-600 ml-1 mr-1"
+        > the ask-my-book-chatbot
+        </a> template.
+      </footer>
     </div>
   )
 }
