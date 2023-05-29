f1e768a10890d912b3b271be1fd24e1b0017f4d9
Your Name <you@example.com>
2023-05-29 11:08:12 -0600

    IDIOMA INGLES

--- ask-my-book-chatbot/_api/prompts.py
+++ ask-my-book-chatbot/_api/prompts.py
@@ -9,7 +9,7 @@ from langchain.chains.prompt_selector import (
     is_chat_model,
 )
 
-CONDENSE_QUESTION_PROMPT_TEMPLATE = """Dada la siguiente conversación y una pregunta de seguimiento, reformula la pregunta de seguimiento para que sea una pregunta independiente.
+CONDENSE_QUESTION_PROMPT_TEMPLATE = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
         Chat History:
         {chat_history}
@@ -20,7 +20,7 @@ CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(
 )
 
 
-prompt_template = """Utiliza los siguientes elementos de contexto para responder la pregunta al final. Si no puedes responder la pregunta con el CONTEXTO proporcionado, simplemente di que no sabes, no intentes inventar una respuesta.
+prompt_template = """Use the following pieces of context to answer the question at the end. If you cannot answer the question with the given CONTEXT, just say that you don't know, don't try to make up an answer.
 
 context: {context}
 
@@ -31,7 +31,8 @@ PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 
-system_template = """Utiliza los siguientes elementos de contexto para responder la pregunta del usuario. Si no puedes responder la pregunta con el contexto proporcionado, simplemente di que no sabes, no intentes inventar una respuesta.
+system_template = """Use the following pieces of context to answer the users question. 
+If you cannot answer the question with the given context, just say that you don't know, don't try to make up an answer.
 ----------------
 {context}"""
 messages = [
