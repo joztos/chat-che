dcaafa45b5abf0df0b196ee351420e31e907e018
joztos <66192875+joztos@users.noreply.github.com>
2023-09-06 08:01:16 -0600

    tr2

--- ai-chatgpt/steamship/prompt.py
+++ ai-chatgpt/steamship/prompt.py
@@ -4,10 +4,27 @@ https://langchain.readthedocs.io/en/latest/modules/memory/examples/chatgpt_clone
 
 from langchain.prompts import PromptTemplate
 
-_TEMPLATE = """El Asistente es un modelo de lenguaje amplio entrenado por OpenAI.
-El Asistente está diseñado para poder ayudar en una amplia gama de tareas, desde responder preguntas simples hasta proporcionar explicaciones detalladas y discusiones sobre una amplia variedad de temas. Como modelo de lenguaje, el Asistente es capaz de generar texto similar al humano basado en la entrada que recibe, lo que le permite participar en conversaciones de forma natural y proporcionar respuestas coherentes y relevantes para el tema en cuestión.
-El Asistente está en constante aprendizaje y mejora, y sus capacidades están en constante evolución. Es capaz de procesar y comprender grandes cantidades de texto, y puede utilizar este conocimiento para proporcionar respuestas precisas e informativas a una amplia gama de preguntas. Además, el Asistente es capaz de generar su propio texto basado en la entrada que recibe, lo que le permite participar en discusiones y proporcionar explicaciones y descripciones sobre una amplia variedad de temas.
-En general, el Asistente es una herramienta poderosa que puede ayudar en una amplia gama de tareas y proporcionar conocimientos e información valiosa sobre una amplia variedad de temas. Ya sea que necesites ayuda con una pregunta específica o simplemente quieras tener una conversación sobre un tema en particular, el Asistente está aquí para ayudar.
+_TEMPLATE = """La planificación de clase es esencial para garantizar que los estudiantes reciban una educación de calidad. Una planificación efectiva asegura que se cubran todos los puntos importantes, se siga una estructura lógica y se alcancen los objetivos de aprendizaje. 
+
+Título de la Clase: {class_title}
+Objetivos de Aprendizaje:
+{learning_objectives}
+
+Temario:
+{agenda}
+
+Actividades Propuestas:
+{proposed_activities}
+
+Evaluación:
+{assessment_method}
+
+Recursos y Materiales:
+{resources}
+
+Notas adicionales:
+{additional_notes}
+
 {history}
 Human: {human_input}
 Assistant:"""
